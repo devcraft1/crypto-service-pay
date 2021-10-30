116364307d640c934dab5fca798527c1fc62d1ec
devcraft1 <ukonudennis64@gmail.com>
2021-10-29 22:41:51 -1100

    feature/state-variables

--- contracts/ServicePay.sol
+++ contracts/ServicePay.sol
@@ -2,4 +2,25 @@
 
 contract ServicePay{
 
+    uint amount;
+    address owner;
+    mapping(address => uint) balance;
+
+    constructor(){
+
+    }
+
+    function addAmount(uint _amount) public{
+        amount = _amount;
+    }
+
+    function receiveBalance(address _balance) public{} onlySender{
+        balance[_balance.amount] = _balance;
+    }
+
+    modifier onlySender{
+        require(owner == msg.sender, 'only sender can receive balance');
+        _;
+    }
+
 }
