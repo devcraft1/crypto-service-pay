9429c0632a9c2f9fdfb0d20d06c18dedbe308f32
devcraft1 <ukonudennis64@gmail.com>
2021-11-03 10:00:42 +0100

    ATM

---
+++ contracts/ATM.sol
@@ -0,0 +1,41 @@
+pragma solidity >=0.5.13 <0.9.0;
+
+contract ATM {
+    mapping(address => uint256) public balances;
+
+    event Deposit(address sender, uint256 amount);
+    event Withdrawal(address receiver, uint256 amount);
+    event Transfer(address sender, address receiver, uint256 amount);
+
+    function deposit() public payable {
+        emit Deposit(msg.sender, msg.value);
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient funds");
+        emit Withdrawal(msg.sender, amount);
+        balances[msg.sender] -= amount;
+    }
+
+    function transfer(address receiver, uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient funds");
+        emit Transfer(msg.sender, receiver, amount);
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+    }
+
+    // In a Batch
+
+    function transfer(address[] memory receivers, uint256 amount) public {
+        require(
+            balances[msg.sender] >= receivers.length * amount,
+            "Insufficient funds"
+        );
+        for (uint256 i = 0; i < receivers.length; i++) {
+            emit Transfer(msg.sender, receivers[i], amount);
+            balances[msg.sender] -= amount;
+            balances[receivers[i]] += amount;
+        }
+    }
+}
