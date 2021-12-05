888089b05cf9b521031410191106fb2496e13c18
devcraft1 <ukonudennis64@gmail.com>
2021-12-04 19:12:56 -0900

    fix/exchange-removed

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,8 +1,8 @@
-// SPDX-License-Identifier:MIT
-pragma solidity ^0.8.0;
+// // SPDX-License-Identifier:MIT
+// pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Token is ERC721 {
-    constructor() ERC721("ExchangeToken", "EXT") {}
-}
+// contract Token is ERC721 {
+//     constructor() ERC721("ExchangeToken", "EXT") {}
+// }
