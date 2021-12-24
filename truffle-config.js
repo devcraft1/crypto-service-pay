1ed1585bb3f50467943bb6eb2382936516a26806
devcraft1 <ukonudennis64@gmail.com>
2021-12-23 23:47:59 -1000

    fix:refactor

--- truffle-config.js
+++ truffle-config.js
@@ -3,13 +3,7 @@ const { projectId, mnemonic } = require('./secrets.json');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
-  // Network configuration
   networks: {
-    // development: {
-    //   host: "127.0.0.1",     // Localhost (default: none)
-    //   port: 8545,            // Standard Ethereum port (default: none)
-    //   network_id: "*",       // Any network (default: none)
-    // },
     ropsten: {
       provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`),
       network_id: 3,       // Ropsten's id
@@ -20,16 +14,12 @@ module.exports = {
     },
 
   },
-
-  // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000
   },
 
-  // Configure your compilers
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
 
-  // Configure your compilers
   compilers: {
     solc: {
       version: "0.8.9",    // Fetch exact version from solc-bin (default: truffle's version)
